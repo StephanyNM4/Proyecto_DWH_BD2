d662b7514e1d1a2334bf08a660cd6048de8d012c
katerinmayely <katerinmayely@gmail.com>
2024-05-01 12:15:47 -0600

    INSERT_LOG  ACTUALIZADO

--- Scripts/SCRIPTS_DWH/OTROS PROCEDIMIENTOS/PROCEDIMIENTO_INSERT_LOG.sql
+++ Scripts/SCRIPTS_DWH/OTROS PROCEDIMIENTOS/PROCEDIMIENTO_INSERT_LOG.sql
@@ -1,20 +1,23 @@
 CREATE OR REPLACE PROCEDURE P_INSERT_LOG(P_nombre VARCHAR2,
     P_fecha_inicio DATE,
     P_nombre_base VARCHAR2,
-    P_exito VARCHAR) AS
+    P_exito VARCHAR,
+    P_error VARCHAR) AS
 BEGIN
     INSERT INTO tbl_logs (
         nombre,
         fecha_inicio,
         fecha_final,
         nombre_base,
-        exito
+        exito,
+        error
     ) VALUES (
         P_nombre,
         P_fecha_inicio,
         SYSDATE,
         P_nombre_base,
-        P_exito
+        P_exito,
+        P_error
     );
     
     COMMIT;
